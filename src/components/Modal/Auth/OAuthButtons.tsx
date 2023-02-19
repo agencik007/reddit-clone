737ef81d11ef4043541772acfda303938ea9488f
Karol Jasnoch <agencik007@gmail.com>
2023-02-20 00:22:46 +0100

    Added firebase with authentication for register form

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,14 +1,35 @@
 import React from 'react';
-import { Button, Flex, Image } from '@chakra-ui/react';
+import { Button, Flex, Image, Text } from '@chakra-ui/react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { auth } from '@/firebase/clientApp';
 
 const OAuthButtons: React.FC = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
     return (
-        <Flex direction='column' width='100%' mb={4}>
-            <Button variant='oauth' mb={2}>
-                <Image src='/images/googlelogo.png' height='20px' mr={4} alt='google logo'/>
+        <Flex direction="column" width="100%" mb={4}>
+            <Button
+                variant="oauth"
+                isLoading={loading}
+                mb={2}
+                onClick={() => signInWithGoogle()}
+            >
+                <Image
+                    src="/images/googlelogo.png"
+                    height="20px"
+                    mr={4}
+                    alt="google logo"
+                />
                 Continue with Google
             </Button>
-            <Button variant='oauth'>Some Other Provider</Button>
+            <Button variant="oauth">Some Other Provider</Button>
+            {error && (
+                <Text textAlign="center" fontSize="10pt" color="red" mt={2}>
+                    {error.message ===
+                    'Firebase: Error (auth/popup-closed-by-user).'
+                        ? 'Popup closed by user, try again'
+                        : error.message}
+                </Text>
+            )}
         </Flex>
     );
 };
--- src/components/Modal/Auth/SignUp.tsx
+++ src/components/Modal/Auth/SignUp.tsx
@@ -2,17 +2,52 @@ import React, { useState } from 'react';
 import { useSetRecoilState } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
 import { Button, Flex, Input, Text } from '@chakra-ui/react';
+import { auth } from '@/firebase/clientApp';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { FIREBASE_ERRORS } from '@/firebase/errors';
 
 const SignUp: React.FC = () => {
     const setAuthModalState = useSetRecoilState(authModalState);
-    const [singUpForm, setSignUpForm] = useState({
+    const [signUpForm, setSignUpForm] = useState({
         email: '',
         password: '',
         confirmPassword: '',
     });
 
+    const [error, setError] = useState('');
+
+    const [createUserWithEmailAndPassword, user, loading, userError] =
+        useCreateUserWithEmailAndPassword(auth);
+
     // Firebase logic
-    const onSubmit = () => {
+    const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (error) setError('');
+        if (!signUpForm.email.includes('@')) {
+            return setError('Please enter a valid email');
+        }
+        if (signUpForm.password.length < 8) {
+            return setError('Password must be at least 8 characters');
+        }
+        if (signUpForm.password !== signUpForm.confirmPassword) {
+            setError('Passwords do not match');
+            return;
+        }
+        if (!signUpForm.password.match(/[A-Z]/g)) {
+            return setError(
+                'Password should contain at least one upper case letter'
+            );
+        }
+        if (!signUpForm.password.match(/[a-z]/g)) {
+            return setError(
+                'Password should contain at least one lower case letter'
+            );
+        }
+        if (!signUpForm.password.match(/[0-9]/g)) {
+            return setError('Password should contain at least one number');
+        }
+
+        createUserWithEmailAndPassword(signUpForm.email, signUpForm.password);
     };
 
     const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,12 +62,12 @@ const SignUp: React.FC = () => {
         <form onSubmit={onSubmit}>
             <Input
                 required
-                name='email'
-                placeholder='email'
-                type='email'
+                name="email"
+                placeholder="email"
+                type="email"
                 mb={2}
                 onChange={onChange}
-                fontSize='10pt'
+                fontSize="10pt"
                 _placeholder={{ color: 'gray.500' }}
                 _hover={{
                     bg: 'white',
@@ -45,16 +80,16 @@ const SignUp: React.FC = () => {
                     border: '1px solid',
                     borderColor: 'blue.500',
                 }}
-                bg='gray.50'
+                bg="gray.50"
             />
             <Input
                 required
-                name='password'
-                placeholder='password'
-                type='password'
+                name="password"
+                placeholder="password"
+                type="password"
                 onChange={onChange}
                 mb={2}
-                fontSize='10pt'
+                fontSize="10pt"
                 _placeholder={{ color: 'gray.500' }}
                 _hover={{
                     bg: 'white',
@@ -67,16 +102,16 @@ const SignUp: React.FC = () => {
                     border: '1px solid',
                     borderColor: 'blue.500',
                 }}
-                bg='gray.50'
+                bg="gray.50"
             />
             <Input
                 required
-                name='confirmPassword'
-                placeholder='confirm password'
-                type='password'
+                name="confirmPassword"
+                placeholder="confirm password"
+                type="password"
                 onChange={onChange}
                 mb={2}
-                fontSize='10pt'
+                fontSize="10pt"
                 _placeholder={{ color: 'gray.500' }}
                 _hover={{
                     bg: 'white',
@@ -89,17 +124,32 @@ const SignUp: React.FC = () => {
                     border: '1px solid',
                     borderColor: 'blue.500',
                 }}
-                bg='gray.50'
+                bg="gray.50"
             />
-            <Button width={'100%'} height={'36px'} mt={2} mb={2} type='submit'>
+            {(error || userError) && (
+                <Text mb={1} textAlign="center" color="red" fontSize="10pt">
+                    {error ||
+                        FIREBASE_ERRORS[
+                            userError?.message as keyof typeof FIREBASE_ERRORS
+                        ]}
+                </Text>
+            )}
+            <Button
+                width={'100%'}
+                height={'36px'}
+                mt={2}
+                mb={2}
+                type="submit"
+                isLoading={loading}
+            >
                 Sign Up
             </Button>
-            <Flex fontSize='9pt' justifyContent='center'>
+            <Flex fontSize="9pt" justifyContent="center">
                 <Text mr={1}>Already a redditor?</Text>
                 <Text
-                    color='blue.500'
+                    color="blue.500"
                     fontWeight={700}
-                    cursor='pointer'
+                    cursor="pointer"
                     onClick={() =>
                         setAuthModalState((prev) => ({
                             ...prev,

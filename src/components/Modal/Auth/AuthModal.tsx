7814950a09ca6811c988160113e8e3d6660bfcc0
agencik007 <agencik007@gmail.com>
2023-02-19 16:32:02 +0100

    Added Login and Singup modal

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -1,58 +1,61 @@
 import React from 'react';
 import {
-    Flex,
-    Modal,
-    ModalBody,
-    ModalCloseButton,
-    ModalContent,
-    ModalHeader,
-    ModalOverlay,
+	Flex,
+	Modal,
+	ModalBody,
+	ModalCloseButton,
+	ModalContent,
+	ModalHeader,
+	ModalOverlay,
 } from '@chakra-ui/react';
 import { useRecoilState } from 'recoil';
 import { authModalState } from '@/atoms/authModalAtom';
+import AuthInputs from './AuthInputs';
 
 const AuthModal: React.FC = () => {
-    const [modalState, setModalState] = useRecoilState(authModalState);
-    const handleClose = () => {
-        setModalState((prev) => ({
-            ...prev,
-            open: false,
-        }));
-    };
-    return (
-        <>
-            <Modal isOpen={modalState.open} onClose={handleClose}>
-                <ModalOverlay />
-                <ModalContent>
-                    <ModalHeader>
-                        {modalState.view === 'login' && 'Login'}
-                        {modalState.view === 'signup' && 'Sign Up'}
-                        {modalState.view === 'resetPassword' &&
-                            'Reset Password'}
-                    </ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody
-                        display="flex"
-                        flexDirection="column"
-                        alignItems="center"
-                        justifyContent="center"
-                    >
-                        <Flex
-                            direction="column"
-                            alignItems="center"
-                            justifyContent="center"
-                            width="70%"
-                            border="1px solid red"
-                        >
-                            {/*<OAuthButtons />*/}
-                            {/*<AuthInputs />*/}
-                            {/*<ResetPassword />*/}
-                        </Flex>
-                    </ModalBody>
-                </ModalContent>
-            </Modal>
-        </>
-    );
+	const [modalState, setModalState] = useRecoilState(authModalState);
+
+	const handleClose = () => {
+		setModalState((prev) => ({
+			...prev,
+			open: false,
+		}));
+	};
+
+	return (
+		<>
+			<Modal isOpen={modalState.open} onClose={handleClose}>
+				<ModalOverlay />
+				<ModalContent>
+					<ModalHeader textAlign='center'>
+						{modalState.view === 'login' && 'Login'}
+						{modalState.view === 'signup' && 'Sign Up'}
+						{modalState.view === 'resetPassword' &&
+							'Reset Password'}
+					</ModalHeader>
+					<ModalCloseButton />
+					<ModalBody
+						display="flex"
+						flexDirection="column"
+						alignItems="center"
+						justifyContent="center"
+                        pb={6}
+					>
+						<Flex
+							direction="column"
+							alignItems="center"
+							justifyContent="center"
+							width="70%"
+						>
+							<AuthInputs />
+							{/*<OAuthButtons />*/}
+							{/*<ResetPassword />*/}
+						</Flex>
+					</ModalBody>
+				</ModalContent>
+			</Modal>
+		</>
+	);
 };
 
 export default AuthModal;

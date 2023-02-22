6a0c3f67c9b96a98098f70257203db13bd893fda
Karol Jasnoch <agencik007@gmail.com>
2023-02-22 14:41:27 +0100

    Added reset password form

--- src/components/Modal/Auth/AuthModal.tsx
+++ src/components/Modal/Auth/AuthModal.tsx
@@ -15,6 +15,7 @@ import AuthInputs from './AuthInputs';
 import OAuthButtons from './OAuthButtons';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '@/firebase/clientApp';
+import ResetPassword from './ResetPassword';
 
 const AuthModal: React.FC = () => {
     const [modalState, setModalState] = useRecoilState(authModalState);
@@ -30,14 +31,14 @@ const AuthModal: React.FC = () => {
     useEffect(() => {
         if (user) handleClose();
         console.log('user', user);
-    }, [user])
+    }, [user]);
 
     return (
         <>
             <Modal isOpen={modalState.open} onClose={handleClose}>
                 <ModalOverlay />
                 <ModalContent>
-                    <ModalHeader textAlign='center'>
+                    <ModalHeader textAlign="center">
                         {modalState.view === 'login' && 'Login'}
                         {modalState.view === 'signup' && 'Sign Up'}
                         {modalState.view === 'resetPassword' &&
@@ -45,24 +46,30 @@ const AuthModal: React.FC = () => {
                     </ModalHeader>
                     <ModalCloseButton />
                     <ModalBody
-                        display='flex'
-                        flexDirection='column'
-                        alignItems='center'
-                        justifyContent='center'
+                        display="flex"
+                        flexDirection="column"
+                        alignItems="center"
+                        justifyContent="center"
                         pb={6}
                     >
                         <Flex
-                            direction='column'
-                            alignItems='center'
-                            justifyContent='center'
-                            width='70%'
+                            direction="column"
+                            alignItems="center"
+                            justifyContent="center"
+                            width="70%"
                         >
-                            <OAuthButtons />
-                            <Text color='gray.500' fontWeight='700'>
-                                OR
-                            </Text>
-                            <AuthInputs />
-                            {/*<ResetPassword />*/}
+                            {modalState.view === 'login' ||
+                            modalState.view === 'signup' ? (
+                                <>
+                                    <OAuthButtons />
+                                    <Text color="gray.500" fontWeight="700">
+                                        OR
+                                    </Text>
+                                    <AuthInputs />
+                                </>
+                            ) : (
+                                <ResetPassword />
+                            )}
                         </Flex>
                     </ModalBody>
                 </ModalContent>

f7827f1f029bfffb0bd9bb0c6e28b16476cbb7d9
agencik007 <agencik007@gmail.com>
2023-02-23 00:51:53 +0100

    Added new icons to navbar

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react';
 import { Menu, MenuButton, MenuList } from '@chakra-ui/menu';
-import { Flex } from '@chakra-ui/react';
+import { Flex, Icon, Text } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
+import { TiHome } from 'react-icons/ti';
 
 const Directory: React.FC = () => {
     return (
         <Menu>
-            <MenuButton cursor='pointer' padding='0 6px' borderRadius={4} _hover={{outline: '1px solid', outlineColor: 'gray.200'}}>
+            <MenuButton cursor='pointer' padding='0 6px' borderRadius={4}
+                        _hover={{ outline: '1px solid', outlineColor: 'gray.200' }}>
                 <Flex align='center'>
-                    <Flex align='center'></Flex>
+                    <Flex align='center'>
+                        <Icon fontSize={24} mr={{ base: 1, md: 2 }} as={TiHome} />
+                        <Flex display={{ base: 'none', lg: 'flex' }}>
+                            <Text>Home</Text>
+                        </Flex>
+                    </Flex>
+                    <ChevronDownIcon />
                 </Flex>
-                <ChevronDownIcon />
             </MenuButton>
             <MenuList></MenuList>
         </Menu>

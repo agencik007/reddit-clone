1f9c21766257472dbed162c2d97f76cb8e11442f
Karol Jasnoch <agencik007@gmail.com>
2023-03-03 12:23:05 +0100

    Added community create handler

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -19,16 +19,25 @@ import {
 } from '@chakra-ui/react';
 import { HiLockClosed } from 'react-icons/hi';
 import { BsFillEyeFill, BsFillPersonFill } from 'react-icons/bs';
+import { auth, firestore } from '@/firebase/clientApp';
+import { doc, getDoc, serverTimestamp, setDoc } from '@firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 type CreateCommunityModalProps = {
     open: boolean;
     handleClose: () => void;
 };
 
-const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handleClose }) => {
+const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({
+    open,
+    handleClose,
+}) => {
+    const [user] = useAuthState(auth);
     const [communityName, setCommunityName] = useState('');
     const [charsRemaining, setCharsRemaining] = useState(21);
     const [communityType, setCommunityType] = useState('public');
+    const [error, setError] = useState('');
+    const [loading, setLoading] = useState(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.value.length > 21) return;
@@ -37,19 +46,62 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
     };
 
     const onCommunityTypeChange = (
-        event: React.ChangeEvent<HTMLInputElement>,
+        event: React.ChangeEvent<HTMLInputElement>
     ) => {
         setCommunityType(event.target.name);
     };
 
+    const handleCreateCommunity = async () => {
+        // validate the community name
+        const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+        if (format.test(communityName) || communityName.length < 3) {
+            setError(
+                'Community name must be between 3-21 characters, and can only obtain letters, numbers and underscore.'
+            );
+            return;
+        }
+        setLoading(true);
+
+        try {
+            const communityDocRef = doc(
+                firestore,
+                'communities',
+                communityName
+            );
+
+            // Check if the community exists in db
+            const communityDoc = await getDoc(communityDocRef);
+
+            if (communityDoc.exists()) {
+                throw new Error(
+                    `Sorry, the r/${communityName} is already taken, try another one`
+                );
+                return;
+            }
+
+            // create the document
+            await setDoc(communityDocRef, {
+                creatorId: user?.uid,
+                createdAt: serverTimestamp(),
+                numberOfMembers: 1,
+                privacyType: communityType,
+            });
+        } catch (error: any) {
+            console.log('handleCreateCommunity error', error);
+            setError(error.message);
+        }
+
+        setLoading(false);
+    };
+
     return (
         <>
-            <Modal isOpen={open} onClose={handleClose} size='lg'>
+            <Modal isOpen={open} onClose={handleClose} size="lg">
                 <ModalOverlay />
                 <ModalContent>
                     <ModalHeader
-                        display='flex'
-                        flexDirection='column'
+                        display="flex"
+                        flexDirection="column"
                         padding={3}
                         fontSize={15}
                     >
@@ -58,40 +110,43 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
                     <Box pl={3} pr={3}>
                         <ModalCloseButton />
                         <ModalBody
-                            display='flex'
-                            flexDirection='column'
-                            padding='10px 0'
+                            display="flex"
+                            flexDirection="column"
+                            padding="10px 0"
                         >
                             <Text fontWeight={600} fontSize={15}>
                                 Name
                             </Text>
-                            <Text fontSize={11} color='gray.500'>
+                            <Text fontSize={11} color="gray.500">
                                 Community names including capitalization cannot
                                 be changed
                             </Text>
                             <Text
-                                position='relative'
-                                top='28px'
-                                width='20px'
-                                left='10px'
-                                color='gray.400'
+                                position="relative"
+                                top="28px"
+                                width="20px"
+                                left="10px"
+                                color="gray.400"
                             >
                                 /r
                             </Text>
                             <Input
-                                position='relative'
+                                position="relative"
                                 value={communityName}
-                                size='sm'
-                                pl='22px'
+                                size="sm"
+                                pl="22px"
                                 onChange={handleChange}
                             />
                             <Text
-                                fontSize='9pt'
+                                fontSize="9pt"
                                 color={
                                     charsRemaining === 0 ? 'red' : 'gray.500'
                                 }
                             >
                                 {charsRemaining} characters remaining
+                            </Text>
+                            <Text fontSize="9pt" color="red" pt={1}>
+                                {error}
                             </Text>
                             <Box>
                                 <Text fontWeight={600} fontSize={15}>
@@ -99,24 +154,26 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
                                 </Text>
                                 <Stack spacing={2}>
                                     <Checkbox
-                                        name='public'
+                                        name="public"
                                         isChecked={communityType === 'public'}
                                         onChange={onCommunityTypeChange}
                                     >
-                                        <Flex
-                                            align='center'
-                                        >
+                                        <Flex align="center">
                                             <Icon
                                                 as={BsFillPersonFill}
-                                                color='gray.500'
+                                                color="gray.500"
                                                 mr={2}
                                             />
-                                            <Text fontSize='10pt' mr={1} width='65px'>
+                                            <Text
+                                                fontSize="10pt"
+                                                mr={1}
+                                                width="65px"
+                                            >
                                                 Public
                                             </Text>
                                             <Text
-                                                fontSize='8pt'
-                                                color='gray.500'
+                                                fontSize="8pt"
+                                                color="gray.500"
                                             >
                                                 Anyone can view, post and
                                                 comment to this community
@@ -124,24 +181,28 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
                                         </Flex>
                                     </Checkbox>
                                     <Checkbox
-                                        name='restricted'
+                                        name="restricted"
                                         isChecked={
                                             communityType === 'restricted'
                                         }
                                         onChange={onCommunityTypeChange}
                                     >
-                                        <Flex align='center'>
+                                        <Flex align="center">
                                             <Icon
                                                 as={BsFillEyeFill}
-                                                color='gray.500'
+                                                color="gray.500"
                                                 mr={2}
                                             />
-                                            <Text fontSize='10pt' mr={1} width='65px'>
+                                            <Text
+                                                fontSize="10pt"
+                                                mr={1}
+                                                width="65px"
+                                            >
                                                 Restricted
                                             </Text>
                                             <Text
-                                                fontSize='8pt'
-                                                color='gray.500'
+                                                fontSize="8pt"
+                                                color="gray.500"
                                             >
                                                 Anyone can view this community,
                                                 but only approved users can post
@@ -149,22 +210,26 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
                                         </Flex>
                                     </Checkbox>
                                     <Checkbox
-                                        name='private'
+                                        name="private"
                                         isChecked={communityType === 'private'}
                                         onChange={onCommunityTypeChange}
                                     >
-                                        <Flex align='center'>
+                                        <Flex align="center">
                                             <Icon
                                                 as={HiLockClosed}
-                                                color='gray.500'
+                                                color="gray.500"
                                                 mr={2}
                                             />
-                                            <Text fontSize='10pt' mr={1} width='65px'>
+                                            <Text
+                                                fontSize="10pt"
+                                                mr={1}
+                                                width="65px"
+                                            >
                                                 Private
                                             </Text>
                                             <Text
-                                                fontSize='8pt'
-                                                color='gray.500'
+                                                fontSize="8pt"
+                                                color="gray.500"
                                             >
                                                 Only approved users can view and
                                                 submit to this community
@@ -175,12 +240,22 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
                             </Box>
                         </ModalBody>
                     </Box>
-                    <ModalFooter bg='gray.100' borderRadius='0px 0px 10px 10px'>
-                        <Button variant='outline' height='30px' mr={3} onClick={handleClose}>
+                    <ModalFooter bg="gray.100" borderRadius="0px 0px 10px 10px">
+                        <Button
+                            variant="outline"
+                            height="30px"
+                            mr={3}
+                            onClick={handleClose}
+                        >
                             Cancel
                         </Button>
-                        <Button height='30px' onClick={() => {
-                        }}>Create Community</Button>
+                        <Button
+                            height="30px"
+                            onClick={handleCreateCommunity}
+                            isLoading={loading}
+                        >
+                            Create Community
+                        </Button>
                     </ModalFooter>
                 </ModalContent>
             </Modal>
